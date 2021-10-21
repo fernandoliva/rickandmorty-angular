87717e465793e3c38e66483969a4559fe1975bf8
Fernando de la Oliva <fernandoliva9@gmail.com>
2021-10-21 18:02:38 +0200

    add routes with Lazy Loading and links on navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+  {path: "characters", loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersModule)},
+  {path: "episodes", loadChildren: () => import('./pages/episodes/episodes.module').then(m => m.EpisodesModule)},
+  {path: "locations", loadChildren: () => import('./pages/locations/locations.module').then(m => m.LocationsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/characters/characters-routing.module.ts
+++ src/app/pages/characters/characters-routing.module.ts
@@ -1,7 +1,12 @@
+import { CharactersComponent } from './characters.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: CharactersComponent},
+  {path: "numero", component: CharacterDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
